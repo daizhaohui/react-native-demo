bd2a4ac1f3977a8bfd4b91810fde3fcfcb44d90d
daizhaohui <daizhaohui@wanda.cn>
2018-12-27 17:52:43 +0800

    ok

--- App.js
+++ App.js
@@ -7,11 +7,19 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View, AppState } from "react-native";
 import Login from "./src/components/login";
+import codePush from "react-native-code-push";
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentDidMount() {
+    //codePush.sync();
+    AppState.addEventListener("change", newState => {
+      newState === "active" && codePush.sync();
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
